64fed27c4cc9cdaec51b927b2c049cfc35146373
dirk ruschhaupt <dirkruschhaupt@gmail.com>
2018-10-03 13:42:22 -0500

    font sizing

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import './customers.css';
 //import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-let area = this.area.value;
-let number = this.number.value;
+// let area = this.area.value;
+// let number = this.number.value;
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * Math.floor(max));
+//   }
 
-function componentDidMount(area, number) {
-    let i = 0;
-    let location = [];
-    if(i < number) {
-        let j = getRandomInt(`${area.length}`);
-        fetch(`/${area}/id:${j}`)
-         .then(res => res.json())
-         .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
-        i++;
-    }
+// function componentDidMount(area, number) {
+//     let i = 0;
+//     let location = [];
+//     if(i < number) {
+//         let j = getRandomInt(`${area.length}`);
+//         fetch(`/${area}/id:${j}`)
+//          .then(res => res.json())
+//          .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
+//         i++;
+//     }
     
-}
+// }
 
 class Customers extends Component {
   constructor() {
@@ -30,11 +30,11 @@ class Customers extends Component {
     };
   }
 
-//   componentDidMount() {
-//     fetch('/api/customers')
-//       .then(res => res.json())
-//       .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
-//   }
+  componentDidMount() {
+    fetch('/api/customers')
+      .then(res => res.json())
+      .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
+  }
 
    render() {
      return (
